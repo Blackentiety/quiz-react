ee80419604357538d1bfdc9ca97eea92231b7d58
lukas <lukas.boudinl@gmail.com>
2026-02-22 00:19:52 +0100

    fix classname

--- src/component/header.jsx
+++ src/component/header.jsx
@@ -1,17 +1,18 @@
-import {Link} from 'react-router'
+import { Link } from 'react-router'
+import './header.module.css'
 function Header() {
   return (
-  <>
-  
-<header>
-      <nav>
-        <Link to="/">Accueil</Link>
-        <Link to="/quiz">Quiz</Link>
-        <Link to="/score">Score</Link>
-      </nav>
-</ header>
-  </>
+    <>
+
+      <header className='header'>
+        <nav className='nav'>
+          <Link to="/">Accueil</Link>
+          <Link to="/quiz">Quiz</Link>
+          <Link to="/score">Score</Link>
+        </nav>
+      </ header>
+    </>
   )
-  
+
 }
 export default Header;
