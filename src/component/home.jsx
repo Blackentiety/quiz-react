63734478f95c927440db57f45ae8754352cd20d7
lukas <lukas.boudinl@gmail.com>
2026-02-23 17:23:50 +0100

    ajout d'un composant select avec refacto du home

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -1,6 +1,7 @@
 import { useNavigate } from 'react-router';
 import { categories, difficulties } from '../data/quizData.jsx'
 import { useState } from "react";
+import Select from "./Select.jsx"
 
 const Home = () => {
   const [selectedCat, setSelectedCat] = useState("");
@@ -18,21 +19,22 @@ const Home = () => {
       <h1>Quiz App</h1>
 
       {/* Sélecteur de catégorie  */}
-      <select onChange={(e) => setSelectedCat(e.target.value)}>
-        <option value="">--Choisir une catégorie--</option>
-        {categories.map(cat => (
-          <option key={cat.id} value={cat.id}>{cat.name}</option>
-        ))}
-      </select>
+      <Select
+        setter={setSelectedCat}
+        data={categories}
+        valueKey="id"
+        labelKey="name"
+        defaultText="Choisir une catégorie"
+      />
 
-      {/* Sélecteur de difficulté [cite: 34] */}
-      <select onChange={(e) => setSelectedDiff(e.target.value)}>
-        <option value="">--Choisir une difficulté--</option>
-        {difficulties.map(diff => (
-          <option key={diff.value} value={diff.value}>{diff.name}</option>
-        ))}
-      </select>
-
+      {/* Sélecteur de difficulté */}
+      <Select
+        setter={setSelectedDiff}
+        data={difficulties}
+        valueKey="value"
+        labelKey="name"
+        defaultText="Choisir une difficulté"
+      />
       <button onClick={handleStart} disabled={!canStart}>
         Démarrer le Quiz
       </button>
