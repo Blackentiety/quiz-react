5f084c8b303b4ff562429d141fb6ea5aba4be4b6
lukas <lukas.boudinl@gmail.com>
2026-02-23 17:36:02 +0100

    correction

--- src/component/Select.jsx
+++ src/component/Select.jsx
@@ -1,11 +1,8 @@
-// On renomme 'function' en 'setter' et on utilise les crochets []
 function Select({ setter, data, valueKey, labelKey, defaultText }) {
   return (
     <select onChange={(e) => setter(e.target.value)}>
-      {/* Correction de la valeur par défaut */}
       <option value="">-- {defaultText} --</option>
 
-      {/* Correction de la boucle map */}
       {data.map((item) => (
         <option key={item[valueKey]} value={item[valueKey]}>
           {item[labelKey]}
